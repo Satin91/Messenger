9318d86d2ab52be403677ebc0bea9db0afa4858b
Satin91 <cool19912015@gmail.com>
2023-06-08 22:55:57 +0300

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Артур Кулик on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
